6d099b21650c1ffd5b133af44477494bbb2109ea
Piotr Stróżyk <93133284+piotrstrozyk@users.noreply.github.com>
2024-05-23 21:41:26 +0200

    fix

--- Skrot/zad2/diff.py
+++ Skrot/zad2/diff.py
@@ -1,3 +1,5 @@
+#Piotr Stróżyk
+
 def diff():
     hex1 = 'd9f1054cb6a2ca9d8fa7696f0af5d8a8f4a5b2f5dc88adb180257dca63b6cb4c2206d79a92de726e6a0500c02f44c176f7ef2bf584d86ca4d6216c1b425370c2'
     hex2 = '51952573fc09a3d6cd2009287624c9f40d5005866bd92b73b871486b9e721ce3b99dbab322fce1f72c689409ce4e175f882093efecbf98e917d80db218bc4564'
@@ -9,6 +11,6 @@ def diff():
     roznica = ([x == y for (x, y) in zip(bin1, bin2)].count(False))
     procent = (float(roznica)/float(dlugosc))*100
 
-    print("%d / %d, procentowo: %.2f%%." % (roznica, dlugosc, procent))
+    print("%d z %d, procentowo: %.2f%%." % (roznica, dlugosc, procent))
 
 diff()
